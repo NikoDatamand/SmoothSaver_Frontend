f32ef6061af2f17d68d1c74833dbce4e4a3de420
NikoDatamand <113106039+NikoDatamand@users.noreply.github.com>
2023-12-08 13:06:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://smoothsaver.azurewebsites.net/api"
+export const API_URL = "https://smoothsaversoftware.azurewebsites.net/api"
 //export const API_URL = "http://localhost:8080/api"
 //export const API_URL = "https://smooth-saver.mastore.repl.co/api"
 
